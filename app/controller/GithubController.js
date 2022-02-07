cb913eaf73b85ef59d4c7b1424b8047471b53caf
Pop Marius <mariuspop@pitechnologies.ro>
2022-02-07 17:41:58 +0200

    Added a new allowed branch

--- app/controller/GithubController.js
+++ app/controller/GithubController.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const { createHmac, timingSafeEqual } = require('crypto');
 
-const allowedBranches = ['develop', 'recette', 'test-pipeline'];
+const allowedBranches = ['develop', 'recette', 'test-pipeline', 'pipeline'];
 
 const app = express();
 
