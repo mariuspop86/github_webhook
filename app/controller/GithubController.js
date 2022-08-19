bc4042f02384621701a11cd15b9373112820f189
Marius Pop <33959561+mariuspop86@users.noreply.github.com>
2022-08-19 17:34:32 +0300

    Added action endpoint

--- app/controller/GithubController.js
+++ app/controller/GithubController.js
@@ -24,81 +24,81 @@ app.use('/payload?2', (req, res, next) => {
 }).post('/payload', async (req, res) => {
 	const { host, reposAPI, runsAPI } = github;
 	await Commit.create({payload:req.body});
-	const { ref, repository: { full_name }, sender: { login, html_url }, commits, head_commit } = req.body;
-	const user_html_url = html_url;
-	let commit_url = commits[0]?.url;
-	let author_name = commits[0]?.committer?.name;
-	console.log(commit_url, author_name);
-	if (!commit_url || !author_name) {
-		commit_url = head_commit?.url;
-		author_name = head_commit?.committer?.name;
-	}
-	console.log(commit_url, author_name);
-	const [,,branch] = ref.split('/');
-	
-	if (!allowedBranches.includes(branch)) {
-		console.log(`Branch ${branch} from ${full_name} repo does not match`);
-    res.send('nok');
-
-    return;
-	}
-	try {
-		await axios.post(host + reposAPI,
-			{
-				"event_type": full_name,
-				"client_payload": {"branch": branch}
-			},
-			{
-				headers: {
-					'Authorization': `token ${ptaToken}`
-				}
-			});
-		
-		setTimeout(async () => {
-			try {
-				const res = await axios.get(host+runsAPI, {
-					headers: {
-						'Authorization': `token ${ptaToken}`
-					}
-				});
-
-				const { workflow_runs } = res.data;
-				const [firstWorkflow] = workflow_runs;
-				const { id, html_url, name } = firstWorkflow;
-				const data = {
-					user: { login, user_html_url, author_name },
-					workflow: { id, html_url, name, commit_url }
-				}
-        await Workflow.create(data);
-			} catch (e) {
-				console.log(e.message);
-			}
-		}, 1500)
-	} catch (e) {
-		console.log(e.message);
-	}
+	// const { ref, repository: { full_name }, sender: { login, html_url }, commits, head_commit } = req.body;
+	// const user_html_url = html_url;
+	// let commit_url = commits[0]?.url;
+	// let author_name = commits[0]?.committer?.name;
+	// console.log(commit_url, author_name);
+	// if (!commit_url || !author_name) {
+	// 	commit_url = head_commit?.url;
+	// 	author_name = head_commit?.committer?.name;
+	// }
+	// console.log(commit_url, author_name);
+	// const [,,branch] = ref.split('/');
+	//
+	// if (!allowedBranches.includes(branch)) {
+	// 	console.log(`Branch ${branch} from ${full_name} repo does not match`);
+  //   res.send('nok');
+	//
+  //   return;
+	// }
+	// try {
+	// 	await axios.post(host + reposAPI,
+	// 		{
+	// 			"event_type": full_name,
+	// 			"client_payload": {"branch": branch}
+	// 		},
+	// 		{
+	// 			headers: {
+	// 				'Authorization': `token ${ptaToken}`
+	// 			}
+	// 		});
+	//	
+	// 	setTimeout(async () => {
+	// 		try {
+	// 			const res = await axios.get(host+runsAPI, {
+	// 				headers: {
+	// 					'Authorization': `token ${ptaToken}`
+	// 				}
+	// 			});
+	//
+	// 			const { workflow_runs } = res.data;
+	// 			const [firstWorkflow] = workflow_runs;
+	// 			const { id, html_url, name } = firstWorkflow;
+	// 			const data = {
+	// 				user: { login, user_html_url, author_name },
+	// 				workflow: { id, html_url, name, commit_url }
+	// 			}
+  //       await Workflow.create(data);
+	// 		} catch (e) {
+	// 			console.log(e.message);
+	// 		}
+	// 	}, 1500)
+	// } catch (e) {
+	// 	console.log(e.message);
+	// }
 
   res.send('ok');
 }).post('/payload2',async (req, res) => {
 	await Commit.create({payload2:req.body});
-	const { action, workflow_run: { id, name, conclusion, html_url } } = req.body;
-	const e = await Workflow.get(id);
-	
-	let text = ` - workflow <${html_url}|*${name}*> completed with *${conclusion}* - `
-	if (e) {
-		const {user: {user_html_url, author_name},workflow: { commit_url } } = e.data
-		text += ` <${user_html_url}|*${author_name}*> pushed <${commit_url}|*commit*> `;
-	}
-	
-	if (action==='completed') {
-		axios.post(slack_url,
-			{
-				"text": text
-			})
-			.catch(error => {
-				console.log('error', error);
-			});
-	}
+	// const { action, workflow_run: { id, name, conclusion, html_url } } = req.body;
+	// const e = await Workflow.get(id);
+	//
+	// let text = ` - workflow <${html_url}|*${name}*> completed with *${conclusion}* - `
+	// if (e) {
+	// 	const {user: {user_html_url, author_name},workflow: { commit_url } } = e.data
+	// 	text += ` <${user_html_url}|*${author_name}*> pushed <${commit_url}|*commit*> `;
+	// }
+	//
+	// if (action==='completed') {
+	// 	axios.post(slack_url,
+	// 		{
+	// 			"text": text
+	// 		})
+	// 		.catch(error => {
+	// 			console.log('error', error);
+	// 		});
+	// }
 	
 	res.send('ok');
 }).post('/action',async (req, res) => {
